e353d60185dde16b47d83ae07726f65c5ce03a98
Kevin <kevinho8866@hotmail.com>
2017-03-10 09:23:11 -0300

    Update Paciente.cpp

--- Paciente/Paciente.cpp
+++ Paciente/Paciente.cpp
@@ -1,31 +1,27 @@
-#include "Paciente.h"
-#include <stdio.h>>
+#include "paciente.h"
+#include <stdio.h>
 #include <conio.h>
+#include <math.h>
 
 void Paciente::entrada(void) {
-
-	printf("Digite o nome do paciente: ");
-	fgets(nome,50,stdin);
-
-	printf("Digite a idade do paciente: ");
-	scanf("%f",idade);
-
-	printf("Digite o peso do paciente: ");
-	scanf("%f",peso);
-
-	printf("Digite a altura do paciente: ");
-	scanf("%f",altura);
-
+    printf("Digite o nome do paciente: ");
+    gets(nome);
+    printf("Digite a idade do paciente: ");
+    scanf("%i",&idade);
+    printf("Digite o peso do paciente: ");
+    scanf("%f",&peso);
+    printf("Digite a altura do paciente: ");
+    scanf("%f",&altura);
 }
 
 void Paciente::imprime(void) {
-
-
-
+    printf("\nNome: %s\n",nome);
+    printf("Idade: %d\n",idade);
+    printf("Peso: %f\n",peso);
+    printf("Altura: %f\n",altura);
 }
 
 float Paciente::calcula_IMC(void) {
-
-
-
+    float IMC = peso/pow(altura,2);
+    return IMC;
 }

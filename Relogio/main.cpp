b302928afd14ee25cf87eef5977501ca91191d6a
Kevin <kevinho8866@hotmail.com>
2017-03-10 08:56:18 -0300

    Update main.cpp

--- Relogio/main.cpp
+++ Relogio/main.cpp
@@ -1,9 +1,17 @@
-#include <stdio.h>
+#include <iostream>
 #include "relogio.h"
 
+using namespace std;
+
 int main(int argc, char *argv[]){
+
     Relogio a;
+
     a.entrada();
     a.imprime();
 
+    for (int i = 0; i < 2000; i++){
+        a.incrementa_1sec();
+    }
+    a.imprime();
 }

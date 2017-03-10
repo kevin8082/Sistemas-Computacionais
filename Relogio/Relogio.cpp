1b0ac7458d9a2da02993d5cdd3c8381b9a60e657
Kevin <kevinho8866@hotmail.com>
2017-03-10 08:47:54 -0300

    Create Relogio.cpp

---
+++ Relogio/Relogio.cpp
@@ -0,0 +1,30 @@
+#include "relogio.h"
+#include<stdio.h>
+
+void Relogio::entrada(void){
+    printf("Digite a Hora: ");
+    scanf("%d",&hora);
+    printf("Digite os minutos: ");
+    scanf("%d",&min);
+    printf("Digite os segundos: ");
+    scanf("%d",&sec);
+}
+
+void Relogio::imprime(void){
+    printf("A hora é: %d:%d:%d\n",hora,min,sec);
+}
+
+void Relogio::incrementa_1sec(void){
+    sec +=sec;
+    if (sec = 60){
+        sec = 0;
+        min++;
+        if (min = 60){
+            min = 0;
+            hora++;
+            if (hora >= 24){
+                hora = 0;
+            }
+        }
+    }
+}

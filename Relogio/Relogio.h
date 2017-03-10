6781f7f92214854b0812ebdf6f52b67beef6691a
Kevin <kevinho8866@hotmail.com>
2017-03-10 08:47:38 -0300

    Create Relogio.h

---
+++ Relogio/Relogio.h
@@ -0,0 +1,15 @@
+#ifndef RELOGIO_H
+#define RELOGIO_H
+
+class Relogio {
+        private:
+        int hora;
+        int min;
+        int sec;
+       public:
+        void entrada(void);
+        void imprime(void);
+        void incrementa_1sec(void);
+}
+
+#endif // RELOGIO_H

de1f755e28345c7dbd09a5c3ccf0fe01eada6543
Kevin <kevinho8866@hotmail.com>
2017-03-24 09:29:20 -0300

    Create empresa.cpp

---
+++ Empresa/empresa.cpp
@@ -0,0 +1,17 @@
+#include "empresa.h"
+
+char * Empresa::getNome(){
+    Return nome;
+}
+
+void Empresa::setNome(char *nome){
+    strcpy(this->nome,nome);
+}
+
+char * Empresa::getCNPJ(){
+    Return CNPJ;
+}
+
+void Empresa::setNome(char *CNPJ){
+    strcpy(this->CNPJ,CNPJ);
+}

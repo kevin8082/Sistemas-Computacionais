2ce84ccf11978dbfc4b10dc2bdb1761a01fc4c51
Kevin <kevinho8866@hotmail.com>
2017-03-24 09:30:13 -0300

    Create funcionario.cpp

---
+++ Empresa/funcionario.cpp
@@ -0,0 +1,25 @@
+#include "funcionario.h"
+
+char * Funcionario::getNome(){
+    Return nome;
+}
+
+void Funcionario::setNome(char *nome){
+    strcpy(this->nome,nome);
+}
+
+int * Funcionario::getSalario(){
+    Return salario;
+}
+
+void Funcionario::setSalario(int * salario){
+    this->salario,salario;
+}
+
+char * Funcionario::getData_Admissao(){
+    Return data_admissao;
+}
+
+void Funcionario::setData_Admissao(char * data_admissao){
+    strcpy(this->data_admissao,data_admissao);
+}

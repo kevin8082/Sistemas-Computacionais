4749a690f7f998746c05c54591515fa8d18e206a
Kevin <kevinho8866@hotmail.com>
2017-03-24 09:36:41 -0300

    Update empresa.h

--- Empresa/empresa.h
+++ Empresa/empresa.h
@@ -1,11 +1,14 @@
 #ifndef EMPRESA_H
 #define EMPRESA_H
 
+#include "funcionario.h"
+
 class Empresa{
 
 private:
     char nome[30];
     char CNPJ[18];
+    Funcionario funcionarios[100];
 public:
     Empresa();
     char * getNome();

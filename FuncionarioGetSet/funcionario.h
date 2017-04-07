08fa59c58c285974bbfb56f3fefeeda6dcff2ca0
Kevin <kevinho8866@hotmail.com>
2017-04-07 09:24:49 -0300

    Create funcionario.h

---
+++ FuncionarioGetSet/funcionario.h
@@ -0,0 +1,21 @@
+#ifndef FUNCIONARIO_H
+#define FUNCIONARIO_H
+
+class Funcionario{
+
+private:
+    char nome[30];
+    int salario;
+    char data_admissao[10];
+public:
+    Funcionario();
+    ~Funcionario();
+    char * getNome();
+    void setNome(char *nome);
+    int getSalario();
+    void setSalario(int *salario);
+    char * getData_Admissao();
+    void setData_Admissao(char *data_admissao);
+};
+
+#endif // FUNCIONARIO_H

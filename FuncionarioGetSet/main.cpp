a84b83df4c6ad3022ce47b49f7359606273bc509
Kevin <kevinho8866@hotmail.com>
2017-04-07 09:35:56 -0300

    Update main.cpp

--- FuncionarioGetSet/main.cpp
+++ FuncionarioGetSet/main.cpp
@@ -6,8 +6,21 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     Funcionario x;
+    char tempc[50];
+    int tempi;
 
+    printf("Nome do funcionário: ");
+    gets_s(tempc);
+    x.setNome(tempc);
+    printf("Salário do funcionário: ");
+    scanf("%d",tempi);
+    x.setSalario(tempi);
+    printf("Data de Admissão do funcionário: ");
+    gets_s(tempc);
+    x.setData_Admissao(tempc);
 
+    x.imprime();
 
+    system("pause");
     return 0;
 }

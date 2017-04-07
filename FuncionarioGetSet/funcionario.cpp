471d76ff45a787d7e393cd0850b1d765b2a4de5d
Kevin <kevinho8866@hotmail.com>
2017-04-07 09:25:01 -0300

    Create funcionario.cpp

---
+++ FuncionarioGetSet/funcionario.cpp
@@ -0,0 +1,39 @@
+#include "funcionario.h"
+#include<stdio.h>
+#include<string.h>
+
+Funcionario::Funcionario(){
+    strcpy(nome,"");
+    salario = 0;
+    strcpy(data_admissao,"");
+    printf("Funcionario::Construtor executado!\n");
+}
+
+char * Funcionario::getNome(){
+    return nome;
+}
+
+void Funcionario::setNome(char *nome){
+    strcpy(this->nome,nome);
+}
+
+int Funcionario::getSalario(){
+    return salario;
+}
+
+void Funcionario::setSalario(int *salario){
+    this->salario,salario;
+}
+
+char * Funcionario::getData_Admissao(){
+    return data_admissao;
+}
+
+void Funcionario::setData_Admissao(char * data_admissao){
+    strcpy(this->data_admissao,data_admissao);
+}
+
+Funcionario::~Funcionario()
+{
+    printf("Funcionario::Destrutor executado!\n");
+}

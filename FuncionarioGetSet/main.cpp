d69c28e39760aa417e2dbd46f372b1817c0d25d4
Kevin <kevinho8866@hotmail.com>
2017-04-07 09:42:33 -0300

    Update main.cpp

--- FuncionarioGetSet/main.cpp
+++ FuncionarioGetSet/main.cpp
@@ -10,17 +10,18 @@ int main(int argc, char *argv[])
     int tempi;
 
     printf("Nome do funcionário: ");
+
     gets_s(tempc);
     x.setNome(tempc);
     printf("Salário do funcionário: ");
-    scanf("%d",tempi);
+    scanf("%d",&tempi);
     x.setSalario(tempi);
     printf("Data de Admissão do funcionário: ");
+
     gets_s(tempc);
     x.setData_Admissao(tempc);
 
     x.imprime();
 
-    system("pause");
     return 0;
 }

9096aabaf591e3cbb0330edf3ecd52d14fb208f8
Kevin <kevinho8866@hotmail.com>
2017-05-06 16:07:13 -0300

    Create funcionario.cpp

---
+++ ex3/funcionario.cpp
@@ -0,0 +1,46 @@
+#include "funcionario.h"
+#include<stdio.h>
+#include<string.h>
+
+Funcionario::Funcionario(){
+    strcpy(nome,"");
+    salario = 0;
+    strcpy(data_admissao,"");
+    printf("Funcionario::Construtor executado!\n");
+}
+
+char *Funcionario::getNome(){
+    return nome;
+}
+
+void Funcionario::setNome(char *nome){
+    strcpy(this->nome,nome);
+}
+
+int Funcionario::getSalario(){
+    return salario;
+}
+
+void Funcionario::setSalario(int salario){
+    this->salario=salario;
+}
+
+char *Funcionario::getData_Admissao(){
+    return data_admissao;
+}
+
+void Funcionario::setData_Admissao(char * data_admissao){
+    strcpy(this->data_admissao,data_admissao);
+}
+
+void Funcionario::imprime()
+{
+    printf("Nome: %s\n", getNome());
+    printf("Salario: R$%d\n", getSalario());
+    printf("Data de Admissao: %s\n", getData_Admissao());
+}
+
+Funcionario::~Funcionario()
+{
+    printf("Funcionario %s encerrado!\n",getNome());
+}

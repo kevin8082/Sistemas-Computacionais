058bd51883cd69dbd3d2b33a80db7fa0bf1ac8e3
Kevin <kevinho8866@hotmail.com>
2017-05-06 16:35:06 -0300

    Update main.cpp

--- ex3/main.cpp
+++ ex3/main.cpp
@@ -3,28 +3,19 @@
 
 using namespace std;
 
+void criafunc(Empresa *empresa);
+
 int main(){
     // 1. Crie um objeto dinâmico do tipo Empresa
     Empresa * empresa;
     empresa = new Empresa((char*)"Kevin Enterprises",(char*)"81.334.352/0001-85");
 
     // 2. Adicione 5 funcionários à Empresa
-    Funcionario a;
-    char tempc[50];
-    int tempi;
-
-    printf("Nome do funcionario: ");
-    rewind(stdin);
-    gets(tempc);
-    a.setNome(tempc);
-    printf("Salario do funcionario: ");
-    scanf("%d",&tempi);
-    a.setSalario(tempi);
-    printf("Data de Admissao do funcionario: ");
-    rewind(stdin);
-    gets(tempc);
-    a.setData_Admissao(tempc);
-    empresa->AdicionarFuncionario(a);
+    criafunc(empresa);
+    criafunc(empresa);
+    criafunc(empresa);
+    criafunc(empresa);
+    criafunc(empresa);
 
     // 3. Liste os nomes de todos funcionários da Empresa
     empresa->ListarFuncionarios();
@@ -36,4 +27,23 @@ int main(){
     delete empresa;
 
     return 0;
+}
+
+void criafunc(Empresa *empresa){
+    Funcionario tempfunc;
+    char tempc[50];
+    int tempi;
+
+    printf("Nome do funcionario: ");
+    rewind(stdin);
+    gets(tempc);
+    tempfunc.setNome(tempc);
+    printf("Salario do funcionario: ");
+    scanf("%d",&tempi);
+    tempfunc.setSalario(tempi);
+    printf("Data de Admissao do funcionario: ");
+    rewind(stdin);
+    gets(tempc);
+    tempfunc.setData_Admissao(tempc);
+    empresa->AdicionarFuncionario(tempfunc);
 }
